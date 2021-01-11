50b2d7fba51a06fe3c419d01414bb664d024bbc8
KaqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-11 18:45:38 -0300

    Att 2 home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,46 +1,50 @@
 import "./home.css"
 import { Container, Col, Row } from "react-bootstrap";
 
-import img01 from "../../assets/images/markerDefesa.png";
-import img02 from "../../assets/images/markerNome.png";
-import img03 from "../../assets/images/markerCasaAcolhimento.png";
-import img04 from "../../assets/images/markerJuridico.png";
-import img05 from "../../assets/images/markerP.png";
+import imglogo from "../../assets/images/logoAssegura.png";
+
+import img01 from "../../assets/images/selfDefense.png";
+import img02 from "../../assets/images/doc.png";
+import img03 from "../../assets/images/map.png";
+import img04 from "../../assets/images/shake.png";
+import img05 from "../../assets/images/house.png";
+import img06 from "../../assets/images/law.png";
+import img07 from "../../assets/images/psicologhy.png";
 
 export default () => {
   return (
-    <Container>
+    <Container fluid>
         <Row>
-            <Col xs={2} className="left">left</Col>
-            <Col xs={8} className="middle">
+            <Col xs={3} className="left">left</Col>
+            <Col xs={6} className="middle">
                 <div class="circlenav">
-                    <img src="./images/logoAssegura.png" alt="Logo assegura LGBTQIA+" />
+                    <img src={imglogo} />
                     <ul class="iconsM">
                         <li class="item">
-                            <a href="defesaPessoal.php"><img src="./images/selfDefense.png" alt="house icon" /></a>
+                            <a href="defesaPessoal.php"><img src={img01} /></a>
                         </li>
                         <li class="item">
-                            <a href="nomeSocial.php"><img src="./images/doc.png" alt="law icon" /></a>
+                            <a href="nomeSocial.php"><img src={img02} /></a>
                         </li>
                         <li class="item">
-                            <a href="localizacao.php"><img src="./images/map.png" alt="icon doc" /></a>
+                            <a href="localizacao.php"><img src={img03} /></a>
                         </li>
                         <li class="item">
-                            <a href="ajude.php"><img src="./images/shake.png" alt="icon" /></a>
+                            <a href="ajude.php"><img src={img04} /></a>
                         </li>
                         <li class="item">
-                            <img src="./images/house.png" alt="" />
+                            <img src={img05} />
                         </li>
                         <li class="item">
-                            <img src="./images/law.png" alt="" />
+                            <img src={img06} />
                         </li>
                         <li class="item">
-                            <img src="./images/psicologhy.png" alt="" />
+                            <img src={img07} />
                         </li>
                     </ul>
                 </div>
             </Col>
-            <Col xs={2} className="right">right</Col>
+            <Col xs={3} className="right">right</Col>
         </Row>
     </Container>
   );
