975912dcb284681196b46b7b6f9b43d47ebc34db
Emerson Pereira <emersonope@gmail.com>
2021-01-16 15:24:27 -0300

    ajuste pg cadastro

--- src/pages/cadastro/cadastro.js
+++ src/pages/cadastro/cadastro.js
@@ -8,49 +8,72 @@ export default () => {
     <Form>
         <Container>
         
-            <h1>Por Gentileza Preencha todos os Dados Abaixo:</h1>
+            <h1>Por gentileza preencha todos os dados abaixo:</h1>
 
             <Form.Row>
                 <Form.Group as={Col} controlId="Primeiro Nome">
-                    <Form.Label>Nome</Form.Label>
+                    <Form.Label>Nome:</Form.Label>
                     <Form.Control type="text" placeholder="Digite seu nome" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="Sobrenome">
-                    <Form.Label>Sobrenome</Form.Label>
+                    <Form.Label>Sobrenome:</Form.Label>
                     <Form.Control type="text" placeholder="Digite seu sobrenome" />
                 </Form.Group>
             </Form.Row>
             <Form.Row>
                 <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Email</Form.Label>
+                    <Form.Label>Email:</Form.Label>
                     <Form.Control type="email" placeholder="Digite seu email" />
                 </Form.Group>
 
+                <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label>Tipo de cadastro:</Form.Label>
+                    <Form.Control as="select" defaultValue="Choose...">
+                        <option>Escolha...</option>
+                        <option>Voluntário</option>
+                        <option>Normal</option>
+                    </Form.Control>
+                </Form.Group>
+            </Form.Row>
+
+            <Form.Row>
                 <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Senha</Form.Label>
+                    <Form.Label>Senha:</Form.Label>
+                    <Form.Control type="password" placeholder="Digite sua senha" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Label>Confirme a senha:</Form.Label>
                     <Form.Control type="password" placeholder="Digite sua senha" />
                 </Form.Group>
             </Form.Row>
 
             <Form.Group controlId="formGridAddress1">
-                <Form.Label>Endereço</Form.Label>
+                <Form.Label>Endereço:</Form.Label>
                 <Form.Control placeholder="Ex: Rua xxxx 1234" />
             </Form.Group>
+            
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridAddress2">
+                    <Form.Label>Complemento:</Form.Label>
+                    <Form.Control placeholder="Apartmento, studio, or piso, síto" />
+                </Form.Group>
 
-            <Form.Group controlId="formGridAddress2">
-                <Form.Label>Complemento</Form.Label>
-                <Form.Control placeholder="Apartmento, studio, or piso, síto" />
-            </Form.Group>
+                <Form.Group as={Col}>
+                    <Form.File id="exampleFormControlFile1" label="Escolha uma foto de perfil" />
+                </Form.Group>
+                
+            </Form.Row>
 
             <Form.Row>
                 <Form.Group as={Col} controlId="formGridCity">
-                    <Form.Label>Cidade</Form.Label>
+                    <Form.Label>Cidade:</Form.Label>
                     <Form.Control placeholder="Digite sua cidade"></Form.Control>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridState">
-                    <Form.Label>Estado</Form.Label>
+                    <Form.Label>Estado:</Form.Label>
                     <Form.Control as="select" defaultValue="Choose...">
                         <option>Escolha...</option>
                         <option>São Paulo</option>
@@ -60,22 +83,15 @@ export default () => {
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridZip">
-                    <Form.Label>CEP</Form.Label>
+                    <Form.Label>CEP :</Form.Label>
                     <Form.Control placeholder="0000-000"></Form.Control>
                 </Form.Group>
             </Form.Row>
             
-            <Form.Row>
-                
-                <Form.Group as={Col}>
-                    <Form.File id="exampleFormControlFile1" label="Escolha uma foto de perfil" />
-                </Form.Group>
-                
-                <Button variant="dark" type="submit">
-                    Enviar
-            </Button>
-            </Form.Row>
             
+            <Button variant="dark" type="submit" >
+                    Enviar
+                </Button>
         </Container>
     </Form>
   );
