13a5c7a4a886a23ad1fe4df99f8620c002086a04
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-20 22:18:27 -0300

    Att pag perfil e ajuda psicologica

--- src/pages/userProfile/userProfile.js
+++ src/pages/userProfile/userProfile.js
@@ -21,43 +21,37 @@ export default () => {
         <Col lg={6} >
             <Row className="coluna2">
                 <Form.Row>
-                    <Form.Group as={Col} controlId="Primeiro Nome"  xs={12} lg={6}>
+                    <Form.Group as={Col} controlId="Primeiro Nome"  xs={12} lg={12}>
                         <Form.Label id="formLabel">Nome:</Form.Label>
                         <Form.Control type="text" placeholder="Digite seu nome" disabled />
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="Sobrenome" lg={6}>
+                    <Form.Group as={Col} controlId="Sobrenome" lg={12}>
                         <Form.Label id="formLabel">Sobrenome:</Form.Label>
                         <Form.Control type="text" placeholder="Digite seu sobrenome" disabled />
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="dataNascimento" lg={6}>
+                    <Form.Group as={Col} controlId="dataNascimento" lg={12}>
                         <Form.Label id="formLabel">Data de nascimento:</Form.Label>
                         <Form.Control type="text" placeholder="DD/MM/AAAA" disabled />
                     </Form.Group>
-                </Form.Row>
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail" xs={12}>
+                    <Form.Group as={Col} controlId="formGridEmail" xs={12} lg={12}>
                         <Form.Label id="formLabel">Email:</Form.Label>
                         <Form.Control type="email" placeholder="Digite seu email" />
                     </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridState">
+                    <Form.Group as={Col} controlId="formGridState" lg={12}>
                         <Form.Label id="formLabel">Tipo de cadastro:</Form.Label>
                         <Form.Control as="select" defaultValue="Choose..." disabled>
                             <option>Voluntário</option>
                             <option>Parceiro</option>
                         </Form.Control>
                     </Form.Group>
-                </Form.Row>
-
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Group as={Col} controlId="formGridPassword" lg={12}>
                         <Form.Label id="formLabel">Senha:</Form.Label>
                         <Form.Control type="password" placeholder="Digite sua senha" />
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Group as={Col} controlId="formGridPassword" lg={12}>
                         <Form.Label id="formLabel">Confirme a senha:</Form.Label>
                         <Form.Control type="password" placeholder="Digite sua senha" />
                     </Form.Group>
@@ -66,23 +60,20 @@ export default () => {
             
             <Row className="coluna2">
                 <Form.Row>
-                <Form.Group as={Col} controlId="formGridAddress1" lg={8}>
+                <Form.Group as={Col} controlId="formGridAddress1" lg={12}>
                     <Form.Label id="formLabel">Endereço:</Form.Label>
                     <Form.Control placeholder="Ex: Rua xxxx 1234" />
                 </Form.Group>
-                    <Form.Group as={Col} controlId="formGridAddress2" lg={4}>
+                    <Form.Group as={Col} controlId="formGridAddress2" lg={12}>
                         <Form.Label id="formLabel">Complemento:</Form.Label>
                         <Form.Control placeholder="Apartmento, studio, piso ou sitio" />
                     </Form.Group>
-                </Form.Row>
-
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridCity" xs={12} lg={6}>
+                    <Form.Group as={Col} controlId="formGridCity" xs={12} lg={12}>
                         <Form.Label id="formLabel">Cidade:</Form.Label>
                         <Form.Control placeholder="Digite sua cidade"></Form.Control>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridState" xs={12} lg={6}>
+                    <Form.Group as={Col} controlId="formGridState" xs={12} lg={12}>
                         <Form.Label id="formLabel">Estado:</Form.Label>
                         <Form.Control as="select" defaultValue="Choose...">
                             <option>São Paulo</option>
@@ -91,7 +82,7 @@ export default () => {
                         </Form.Control>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridZip" xs={12} lg={6}>
+                    <Form.Group as={Col} controlId="formGridZip" xs={12} lg={12}>
                         <Form.Label id="formLabel">CEP :</Form.Label>
                         <Form.Control placeholder="0000-000"></Form.Control>
                     </Form.Group>
