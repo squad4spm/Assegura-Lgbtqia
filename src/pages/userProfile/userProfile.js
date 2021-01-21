0099e4f29bbb9faf901ad2065fcff249203b8687
KaiqueLimaNovaes <kaiquenovaes.lima@gmail.com>
2021-01-21 00:41:47 -0300

    Att userP

--- src/pages/userProfile/userProfile.js
+++ src/pages/userProfile/userProfile.js
@@ -2,6 +2,9 @@ import "./userProfile.css"
 import React, {useContext} from 'react';
 import { Container, Col, Row, Image, Button, Form } from "react-bootstrap";
 import { useHistory } from 'react-router-dom';
+import axios from "axios";
+
+import FormEndereco from "./FormEndereco";
 
 import userProfile from "../../assets/images/userProfile.png";
 
@@ -14,7 +17,6 @@ const initialValues = {
 }
 
 export default () => {
-    const history = useHistory();
 
     const [values, setValues] = React.useState(initialValues)
 
@@ -32,6 +34,8 @@ export default () => {
 
        console.log('euu', result)
     }
+
+    const history = useHistory();
 
     return (
     <Container>
@@ -82,43 +86,8 @@ export default () => {
                 </Form.Row>
             </Row>    
             
-            <Row className="coluna2">
-                <Form onSubmit={handleOnEndereco}>
-                    <Form.Row>
-                    <Form.Group as={Col} controlId="formGridAddress1" lg={12}>
-                        <Form.Label id="formLabel">Rua:</Form.Label>
-                        <Form.Control name="rua" type="text" placeholder="Ex: Rua xxxx 1234" onChange={HandleonChange}/>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridAddress1" lg={12}>
-                        <Form.Label id="formLabel">Numero:</Form.Label>
-                        <Form.Control name="numero" type="text" placeholder="Ex: Rua xxxx 1234" onChange={HandleonChange}/>
-                    </Form.Group>
-                        <Form.Group as={Col} controlId="formGridAddress2" lg={12}>
-                            <Form.Label id="formLabel">Bairro:</Form.Label>
-                            <Form.Control name="bairro" type="text" placeholder="Apartmento, studio, piso ou sitio" onChange={HandleonChange}/>
-                        </Form.Group>
-                        <Form.Group as={Col} controlId="formGridCity" xs={12} lg={12}>
-                            <Form.Label id="formLabel">Cidade:</Form.Label>
-                            <Form.Control name="cidade" type="text" placeholder="Digite sua cidade" onChange={HandleonChange}></Form.Control>
-                        </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridState" xs={12} lg={12}>
-                            <Form.Label id="formLabel">Estado:</Form.Label>
-                            <Form.Control name="estado" type="text" as="select" defaultValue="Choose..." onChange={HandleonChange}>
-                                <option>São Paulo</option>
-                                <option>Rio de Janeiro</option>
-                                <option>Outro</option>
-                            </Form.Control>
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formGridZip" xs={12} lg={12}>
-                            <Form.Label id="formLabel">CEP :</Form.Label>
-                            <Form.Control name="cep" placeholder="0000-000"></Form.Control>
-                        </Form.Group>
-                    </Form.Row>
-                    <Button type="submit" className="botaoEdit">Salvar alterações</Button>
-                </Form>
-            </Row>
+            <FormEndereco />
         </Col>
     </Row>
     </Container>
