89ab4aa0f25472ca46737c666639ccc376ee3af4
Elvis Brito <elvisbritodossantos61@gmail.com>
2021-02-18 12:29:33 -0300

    required categorias

--- src/pages/admin/categorias/FormEdit.jsx
+++ src/pages/admin/categorias/FormEdit.jsx
@@ -91,7 +91,7 @@ const FormEdit = ({ history, match }) => {
 
       <form onSubmit={handleSubmit(handleOnSubmit)}>
         <Label>Nome Categoria:</Label>
-        <Input type="text" name="nome" ref={register} />
+        <Input type="text" name="nome" ref={register} required />
         <Button>Enviar</Button>
       </form>
     </Container>
